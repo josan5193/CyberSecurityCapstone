b1a948af3df308640bf4a5893e9735cf3a655780
Dylan Sidani <60626132+dsidani1@users.noreply.github.com>
2021-05-04 23:05:53 -0500

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -1,9 +1,17 @@
 #simple sha256 hash for the input of any string like a name or whatever
 #hashlib module
 import hashlib
+import CreateNameDB
+
+Test1Word = "Test1Word"
+usernum = "usernum"
+presnum = "presnum"
+
 Voter = "Capstone Project"
 #send to hashing alg
-HashedString = hashlib.sha256(Voter.encode())
+HashedString = hashlib.sha256(Test1Word.encode())
+HashedString = hashlib.sha256(usernum.encode())
+HashedString = hashlib.sha256(presnum.encode())
 #printing the hex value just to show what it actually does
 #other functions can be applied other than print obviously
 #whatever function to write to the wb in the excel i/o code works too
